8b35151a809d4de89a203f7ad9e666619d47cd2d
Meghan Bomberger <meghanbomberger@gmail.com>
2021-02-20 15:19:39 -0700

    centering projects

--- routes/ProjectsRouter.js
+++ routes/ProjectsRouter.js
@@ -5,51 +5,51 @@ const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process
 const projectsRouter = express.Router()
 
 projectsRouter.get('/', async (req, res, next) => {
-	try {
-		await base('projects').select({
-			view: "Grid view"
-		}).eachPage(function page(records, fetchNextPage) {
-			const projects = records.map(record => {
-				const skills = record.fields.skill_ids.map((skill, i) => {
-					return {
-						skill_id: skill,
-						skill_name: record.fields.skill_names[i],
-						skill_category: record.fields.skill_categories[i],
-						skill_icon: record.fields.skill_icons[i].thumbnails.full.url
-					}
-				})
+  try {
+    await base('projects').select({
+      view: "Grid view"
+    }).eachPage(function page(records, fetchNextPage) {
+      const projects = records.map(record => {
+        const skills = record.fields.skill_ids.map((skill, i) => {
+          return {
+            skill_id: skill,
+            skill_name: record.fields.skill_names[i],
+            skill_category: record.fields.skill_categories[i],
+            skill_icon: record.fields.skill_icons[i].thumbnails.full.url
+          }
+        })
 
-				return {
-					id: record.id,
-					project_name: record.fields.project_name,
-					skills: skills,
-					desktop_img: record.fields.desktop_img[0].thumbnails.full.url,
-					mobile_img: record.fields.mobile_img[0].thumbnails.full.url,
-					description: record.fields.description,
-					url: record.fields.url,
-					repo: record.fields.repo,
-					contributor: record.fields.contributor
-				}
-			})
+        return {
+          id: record.id,
+          project_name: record.fields.project_name,
+          skills: skills,
+          desktop_img: record.fields.desktop_img[0].thumbnails.full.url,
+          mobile_img: record.fields.mobile_img[0].thumbnails.full.url,
+          description: record.fields.description,
+          url: record.fields.url,
+          repo: record.fields.repo,
+          contributor: record.fields.contributor
+        }
+      })
 
-			res.send({
-				success: true,
-				projects: projects
-			})
-		}, function done(err) {
-			if (err) {
-				console.error(err)
-				return
-			}
-		})
-	} catch (err) {
-		if (err) {
-			console.error(err)
-		}
-		res.send({
-			success: false
-		})
-	}
+      res.send({
+        success: true,
+        projects: projects
+      })
+    }, function done(err) {
+      if (err) {
+        console.error(err)
+        return
+      }
+    })
+  } catch (err) {
+    if (err) {
+      console.error(err)
+    }
+    res.send({
+      success: false
+    })
+  }
 })
 
 module.exports = projectsRouter
